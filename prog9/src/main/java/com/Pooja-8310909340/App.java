d881a02f13c0b10609c55689f6bbc03ebd037a00
Pooja K G <poojakg8055@gmail.com>
2025-03-13 15:13:39 +0530

    azure pipeline example

---
+++ prog9/src/main/java/com/Pooja-8310909340/App.java
@@ -0,0 +1,13 @@
+package com.Pooja-8310909340;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
